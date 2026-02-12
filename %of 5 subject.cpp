11659f2c2fc07281d4524a97e85a246eaa64bb99
priyanshi8904 <25121cn129@glbitm.ac.in>
2026-02-12 22:24:43 +0530

    percentage of 5 subject

---
+++ %of 5 subject.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{float x1=39;
+float x2=36;
+float x3=38;
+float x4=37;
+float p=(x1+x2+x3+x4)/160*100;
+printf("the percentage of the 5 subject is:%f",p);
+return 0;
+}
