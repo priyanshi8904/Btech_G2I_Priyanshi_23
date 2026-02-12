e67f6f18d0851cf8ebf122a72ba7c04328b909c8
priyanshi8904 <25121cn129@glbitm.ac.in>
2026-02-12 20:54:13 +0530

    area of rectangle

---
+++ area of rectangle.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{float length ,breadth;
+printf("enter length");
+scanf("%f",&length);
+ 
+printf("enter breadth");
+scanf("%f",&breadth);
+printf("area is :%f",length*breadth);
+getch();
+}
